3cfaba5a986b2f2b7166239815c7ad3fdb988cb8
AngelLima1910 <angellima200@hotmail.com>
2023-04-27 13:40:00 -0600

    Cronometro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    //Muestra el mensaje de cada cuando se ira actualizando
     public mensaje : string  = '';
     public actualizar(tiempo: number) : void {
         this.mensaje = tiempo + ' (se actualiza cada 10 segundos)';
--- src/app/cronometro/cronometro.component.ts
+++ src/app/cronometro/cronometro.component.ts
@@ -6,20 +6,23 @@ import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
   styleUrls: ['./cronometro.component.css']
 })
 export class CronometroComponent implements OnInit {
+    //Es el inicio el cual va a generar el segundo donde va a iniciar el cronometro
     public segundo : number = 0;
     @Input() public inicio : number = 0;
     @Output() public multiplo10 = new EventEmitter();
-    
+
     ngOnInit(): void {
         this.segundo = this.inicio;
         setInterval(this.actualizarSegundo.bind(this), 1000);
     }
+    //Genera el inicio y ira incrementando por segundo
     private actualizarSegundo() : void {
         this.segundo++;
         if (this.segundo % 10 === 0) {
             this.emitirMultiplo10();
         }
     }
+    //Emite el multiplo de 10 para notificar cada cuando se ira actualizando el cronometro
     private emitirMultiplo10() : void {
         this.multiplo10.emit(this.segundo);
     }
